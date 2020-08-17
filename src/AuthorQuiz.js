155207baad01093b0f1d3a4e120c8e1abcbe63b1
Morten Brudvik <mbr@epsis.com>
2020-08-17 16:20:41 +0200

    First commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+
+function AuthorQuiz(props) {
+  return (
+    <div>Author Quiz</div>
+     
+  );
+}
+
+export default AuthorQuiz;
