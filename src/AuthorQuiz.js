654c762ac66e4131b9c353c8e10deb79418e0564
Morten Brudvik <mbr@epsis.com>
2020-08-20 15:45:41 +0200

    added author data and logic for setting up the page.

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,13 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
 
-function AuthorQuiz(props) {
+function AuthorQuiz({turnData}) {
   return (
-    <div>Author Quiz</div>
-     
+    <div className="container-fluid">
+      <Hero/>
+      <Turn {...turnData} />
+      <Continue/>
+      <Footer/>
+    </div>
   );
+}
+
+function Hero() {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book written by author shown.</p>
+      </div>
+    </div>
+  )
+}
+
+function Turn({author, books}) {
+  return (
+    <div className="row turn" style={{backgroundColor: "white"}} >
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="author"/>        
+      </div>
+      <div className="col-6">
+        {books.map((title) => <Book title={title} key={title}/>)}
+      </div>
+    </div>
+  );
+}
+
+function Book({title}) {
+  return (
+    <div className="answer">
+      <h4>{title}</h4>
+    </div>
+  );
+}
+
+function Continue() {
+  return (<div/>);
+}
+
+function Footer() {
+  return (<div/>);
 }
 
 export default AuthorQuiz;
